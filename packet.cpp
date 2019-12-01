74a1e373b2857ab4838a270b6866559ab42af7cf
Marcus <marcus.casey@outlook.com>
2019-12-01 14:16:35 -0800

    packet basis, need to add more

--- packet.cpp
+++ packet.cpp
@@ -2,17 +2,13 @@
 
 Packet::Packet()
 {
-
 }
 
 Packet::~Packet()
 {
-
-
-
 }
 
-Packet::Packet(const Packet& rhs)
+Packet::Packet(const Packet &rhs)
 {
 
   m_srcID = rhs.m_srcID;
@@ -21,5 +17,87 @@ Packet::Packet(const Packet& rhs)
   m_timeTraversed = rhs.m_timeTraversed;
   m_TTL = rhs.m_TTL;
   m_maxHops = rhs.m_maxHops;
+}
 
+int Packet::getPriority(
+    // no parameters
+)
+{
+  return m_priority;
+}
+
+void Packet::sendToNode(
+    Node *nodeLocation)
+{
+  if (currentNode != nodeLocation)
+  {
+    currentNode = nodeLocation;
+  }
+}
+
+bool Packet::atDestination(
+    // no parameters
+)
+{
+  if (currentNode == destination)
+  {
+    return true;
+  }
+
+  else
+  {
+    return false;
+  }
+}
+
+size_t Packet::getSrcID() const
+{
+
+  return m_srcID;
+}
+
+size_t Packet::getMaxNumOfNodes() const
+{
+  return m_data;
+}
+
+Path Packet::getTimeTraversed() const
+{
+  return m_timeTraversed;
+}
+
+Path Packet::getTTL() const
+{
+  return m_TTL;
+}
+
+Path Packet::getMaxHops() const
+{
+  return m_maxHops;
+}
+
+size_t Packet::setSrcID() const
+{
+
+  return m_srcID;
+}
+
+size_t Packet::setMaxNumOfNodes() const
+{
+  return m_data;
+}
+
+Path Packet::setTimeTraversed() const
+{
+  return m_timeTraversed;
+}
+
+Path Packet::setTTL() const
+{
+  return m_TTL;
+}
+
+Path Packet::setMaxHops() const
+{
+  return m_maxHops;
 }
--- packet.h
+++ packet.h
@@ -1,29 +1,27 @@
 #ifndef PACKET_H_
 #define PACKET_H_
 
+class Packet
+{
 
+public:
+  Packet();
+  Packet(const Packet &rhs);
+  ~Packet();
 
+  // setters and getters
+  int getPriority();
+  void sendToNode(Node *nodeLocation);
+  bool atDestination();
 
-class Packet { 
-
-  public:
-    Packet();
-    Packet(const Packet& rhs);
-    ~Packet();
-    
-    // setters and getters
-
-    //  
-
-
-  private:
-    // header 
-    int m_srcID;
-    int m_destID;
-    int m_data;
-    int m_timeTraversed;
-    int m_TTL;
-    int m_maxHops;
+private:
+  // header
+  int m_srcID;
+  int m_destID;
+  int m_data;
+  int m_timeTraversed;
+  int m_TTL;
+  int m_maxHops;
 };
 
 #endif
